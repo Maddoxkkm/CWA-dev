8eba19cc616215b3c52bb3fa706d55a1da7ed224
Maddoxkkm <maddox.kkm@gmail.com>
2019-07-25 19:43:17 +1000

    renaming folders and moving stuff around

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { request } from './src/request'
+import { request } from './botBackend/request'
 
 const test = request('https://api.worldoftanks.asia/wgn/servers/info/?application_id=71df07a3f5c764028c167d09eec0cd99')
     .then(x => console.log(x))
