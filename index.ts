d12f2ea7255fdbce97d03598f3930ff8f7feef7d
Maddoxkkm <maddox.kkm@gmail.com>
2019-07-26 23:11:38 +1000

    Modified routes for root

--- index.ts
+++ index.ts
@@ -14,7 +14,12 @@ main.use((req, res, next) => {
     next();
 })
 
+main.all('/', (req, res) =>{
+    res.send("Welcome to CWA Homepage, the site is currently under construction.")
+})
+
 main.use('/api/verify/', cwabot.verificationApp)
+main.use((req, res, next) => res.redirect('/'))
 main.listen(3000)
 
 cwabot.startBot();
