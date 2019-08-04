93bfab93a6c1a44a162dabd15e8e4863ce39549c
Maddoxkkm <maddox.kkm@gmail.com>
2019-08-05 00:30:54 +1000

    added cookie-parser, moved code around web modules

--- index.ts
+++ index.ts
@@ -1,25 +1,11 @@
-import express from 'express'
 import CWABot from './botBackend/bot'
 
-import path from 'path';
-
-const main = express();
+import webService from './webService/app'
 
 const cwabot = CWABot;
 
-main.use((req, res, next) => {
-    console.log(req.path)
-    next();
-})
-
-main.use(express.static(path.join(__dirname, "public")))
-
-main.all('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
-})
-
-main.use('/api/verify/', cwabot.verificationApp)
-main.use((req, res, next) => res.redirect('/'))
-main.listen(80)
-
 cwabot.startBot();
+
+webService.use('/api/verify/', cwabot.verificationApp)
+webService.use((req, res, next) => res.redirect('/'))
+webService.listen(80)
--- webService/app.ts
+++ webService/app.ts
@@ -1,4 +1,19 @@
 import express from 'express';
+import path from 'path';
 
-const app = express();
-app.use(express.json())
+const webService = express();
+
+webService.use(express.json());
+
+webService.use((req, res, next) => {
+    console.log(req.path)
+    next();
+})
+
+webService.use(express.static(path.join(__dirname, "public")));
+
+webService.all('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+});
+
+export default webService
